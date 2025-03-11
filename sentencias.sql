e133a47127e38c34a3320a161df9b60434ced6de
solp3rez <solperezramirez1@gmail.com>
2025-03-11 16:54:46 -0300

    juego sql island

---
+++ sentencias.sql
@@ -0,0 +1,16 @@
+1.SELECT *FROM INHABITANT
+2.SELECT *FROM INHABITANT WHERE STATE = "friendly"
+3.SELECT * FROM INHABITANT WHERE STATE = "friendly" AND  job = "weaponsmith" 
+4.SELECT * FROM INHABITANT WHERE  state = "friendly" AND job LIKE "%smith"
+5.SELECT personid from INHABITANT WHERE name like "stranger"
+6.SELECT gold from inhabitant where name = "Stranger" 
+7.SELECT * from   item where owner is null
+8.UPDATE item SET owner = 20 WHERE item = 'coffee cup'
+9.UPDATE item SET owner = 20 WHERE  owner is null 
+10.select slecionar o mostar o lisatr y update modificado where yo digo que quiero filtrar 
+11.SELECT  * from item  WHERE  owner = 20
+12.SELECT  * from INHABITANT  WHERE  STATE = "friendly" AND  job = "dealer" OR job = "merchant"
+13.SELECT  * from item WHERE item = 'ring' AND  item = 'teapot'(MODIFICAR
+)
+14.
+15.
