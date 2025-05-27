0239fbf713efc842e007c411a04c07d3dbb33482
solp3rez <axelweath@gmail.com>
2025-05-27 16:55:11 -0300

    termiandndo chinook

--- ACT-CHINOOK/consignas-Chinook.sql
+++ ACT-CHINOOK/consignas-Chinook.sql
@@ -83,3 +83,39 @@ ORDER BY  a.AlbumId ASC;
 
 
 /*act 7*/
+
+
+SELECT t.TrackId, a.Title as "Album Name", t.Milliseconds as duracion, a.Title as name_disco, ar.name as name_artists
+from albums a 
+INNER JOIN tracks t 
+ON a.AlbumId = t.AlbumId
+INNER JOIN artists ar
+ON a.ArtistId = ar.ArtistId
+ORDER BY ar.name ASC LIMIT 20
+
+/*act 8*/
+SELECT 
+    e.LastName AS apellido_jefe,j.LastName as apellido_empleado, e.title as puesto, count(c.CustomerId) as cant_clientes_que_atiende
+FROM employees e 
+inner join employees j
+on e.EmployeeId = j.ReportsTo
+INNER JOIN customers c
+on c.SupportRepId = j.EmployeeId
+GROUP by j.EmployeeId
+ORDER BY cant_clientes_que_atiende DESC;
+
+/*act 9*/
+
+INSERT into tracks (TrackId,name,MediaTypeId,Milliseconds,UnitPrice)
+VALUES (3504,"ella dijo",1,2,0.99),(3506,"no oned noticed",2,2,1.10),(3508,"drake",3,236,0.99),(3510,"i wonder",4,310,0.99)
+
+/*act 10*/
+
+SELECT t.name
+FROM tracks t 
+WHERE t.name = "ella dijo" or t.name = "no oned noticed" or t.name = "drake" or t.name = "i wonder" 
+
+/*act 11*/
+
+
+
